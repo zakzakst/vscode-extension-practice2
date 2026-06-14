01b4855b0d0cd44695b3bc7edab5b88e1b788e84
zakzakst <e1433104@gmail.com>
2026-06-14 23:01:07 +0900

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
-import { SampleProvider } from "./providers/sample";
+import { contextSampleDisposable } from "@/disposables/context-sample";
 import { sampleDisposable } from "@/disposables/sample";
+import { SampleProvider } from "@/providers/sample";
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   // Disposables
   context.subscriptions.push(sampleDisposable);
+  context.subscriptions.push(contextSampleDisposable);
 
   // Providers
   const sampleProvider = new SampleProvider();
